328adc7ff08d707411e8951edd97570f1d382437
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-05-10 22:29:15 +0300

    Initial Commit

---
+++ CresusApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CresusApp
+//
+//  Created by newAccount on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
